d1cd37f679d92e76dcd6581755382730891e4a5d
Nirali Somia <somia@LA34PREP02.cshl.edu>
2024-07-09 15:03:53 -0400

    frechet distance done

--- helpers.py
+++ helpers.py
@@ -10,11 +10,17 @@ from PL_DeepSTARR import *
 
 samples_file_path = 'samples.npz'
 deepSTARR_data = 'DeepSTARR_data.h5'
-oracle = 'oracle_DeepSTARR_DeepSTARR_data.ckpt' 
+oracle_path = 'oracle_DeepSTARR_DeepSTARR_data.ckpt'
 
-def load_predictions(samples_file_path, deepSTARR_data, oracle):
-    
-    #load samples from .npz file
+class EmbeddingExtractor:
+    def __init__(self):
+        self.embedding = None
+
+    def hook(self, module, input, output):
+        self.embedding = output.detach()
+
+def extract_data(samples_file_path, deepSTARR_data):
+   #load samples from .npz file
     data = load(samples_file_path)
     samples = []
     lst = data.files
@@ -29,17 +35,46 @@ def load_predictions(samples_file_path, deepSTARR_data, oracle):
     #transpose samples to get shape (41186, 4, 249)
     x_synthetic = np.transpose(samples[0], (0, 2, 1))
 
-    #load model
-    ckpt_aug_path = oracle
-    deepstarr = PL_DeepSTARR.load_from_checkpoint(ckpt_aug_path).eval()
-
     #make into tensors
     x_test_tensor = torch.from_numpy(x_test).float()
-    x_synthetic_tensor = torch.from_numpy(x_synthetic).float()
+    x_synthetic_tensor = torch.from_numpy(x_synthetic).float() 
+
+    return x_test_tensor, x_synthetic_tensor
+
+def load_deepstarr(oracle_path):
+    
+    #load model
+    ckpt_aug_path = oracle_path
+    deepstarr = PL_DeepSTARR.load_from_checkpoint(ckpt_aug_path).eval()
+
+    return deepstarr
+
+def load_predictions(x_test_tensor, x_synthetic_tensor, deepstarr):
 
     #run model predictions
     y_hat_test = deepstarr(x_test_tensor)
     y_hat_syn = deepstarr(x_synthetic_tensor)
 
-    #returns numpy arrays of oracle predictions
+    #returns numpy arrays of deepstarr predictions from samples and x test
     return y_hat_test.detach().numpy(), y_hat_syn.detach().numpy()
+
+
+extractor = EmbeddingExtractor()
+def get_penultimate_embeddings(model, x):
+    # Find the penultimate layer
+    for name, module in model.named_modules():
+        if name == 'model.batchnorm6':
+            handle = module.register_forward_hook(extractor.hook)
+            break
+    else:
+        raise ValueError("Could not find 'model.batchnorm6' layer")
+
+    # Forward pass
+    with torch.no_grad():
+        _ = model(x)
+
+    # Remove the hook
+    handle.remove()
+
+    return extractor.embedding
+
---
+++ seq_evals_improved.py
@@ -0,0 +1,72 @@
+import numpy as np
+import scipy
+from scipy import linalg
+
+#############################################################################################
+# Functional similarity: Conditional generation fidelity
+#############################################################################################
+'''
+prerequisite: 
+    - x_synthetic (generated seqeunces with shapes (N,L,A)) 
+    - x_test (observed sequences with shapes (N,L,A)) 
+    - oracle (inference model that maps x to activities)
+
+example:
+    activity1 = oracle.predict(x_synthetic)
+    activity2 = oracle.predict(x_test)
+    mse = conditional_generation_fidelity(activity1, activity2)
+'''
+
+def conditional_generation_fidelity(activity1, activity2):
+    return np.mean((activity1 - activity2)**2)
+
+#############################################################################################
+# Functional similarity: Frechet distance
+#############################################################################################
+'''
+prerequisite: 
+    - x_synthetic (generated seqeunces with shapes (N,L,A)) 
+    - x_test (observed sequences with shapes (N,L,A)) 
+    - oracle_embedding_fun (function that acquires the penultimate embeddings)
+
+example:
+    embeddings1 = oracle_embedding_fun(x_synthetic)
+    embeddings2 = oracle_embedding_fun(x_test)
+    mu1, sigma1 = calculate_activation_statistics(embeddings1)
+    mu2, sigma2 = calculate_activation_statistics(embeddings2)
+    distance = calculate_frechet_distance(mu1, sigma1, mu2, sigma2)
+'''
+
+def calculate_activation_statistics(embeddings):
+    embeddings_d = embeddings.detach().numpy()
+    mu = np.mean(embeddings_d, axis=0)
+    sigma = np.cov(embeddings_d, rowvar=False)
+    return mu, sigma
+
+def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
+    # adapted from https://github.com/mseitzer/pytorch-fid/blob/master/src/pytorch_fid/fid_score.py
+    #Frechet distance: d^2 = ||mu_1 - mu_2||^2 + Tr(C_1 + C_2 - 2*sqrt(C_1*C_2)).
+
+    diff = mu1 - mu2
+
+    # Product might be almost singular
+    covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+    if not np.isfinite(covmean).all():
+        msg = (
+            "fid calculation produces singular product; "
+            "adding %s to diagonal of cov estimates"
+        ) % eps
+        print(msg)
+        offset = np.eye(sigma1.shape[0]) * eps
+        covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+
+    # Numerical error might give slight imaginary component
+    if np.iscomplexobj(covmean):
+        if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+            m = np.max(np.abs(covmean.imag))
+            raise ValueError("Imaginary component {}".format(m))
+        covmean = covmean.real
+
+    tr_covmean = np.trace(covmean)
+
+    return diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean
