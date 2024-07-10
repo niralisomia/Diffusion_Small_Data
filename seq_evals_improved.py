8eb07ee88696f677cc7ebd42bf56d369be16ce81
Yijie Kang <yijiekang@YijiedeMBP.cshl.edu>
2024-07-10 13:55:16 -0400

    k-mer seq eval

--- seq_evals_improved.py
+++ seq_evals_improved.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy
 from scipy import linalg
+from tqdm import tqdm
 
 #############################################################################################
 # Functional similarity: Conditional generation fidelity
@@ -97,3 +98,130 @@ def predictive_distribution_shift(x_synthetic_tensor, x_test_tensor):
 
     #return ks test statistic
     return scipy.stats.ks_2samp(base_indices_syn_f, base_indices_test_f).statistic
+
+    #############################################################################################
+# Sequence similarity: k-mer spectrum shift
+#############################################################################################
+'''
+prerequisite:
+    - x_synthetic (generated seqeunces with shapes (N,L,A)) 
+    - x_test (observed sequences with shapes (N,L,A))
+
+example:
+    kld, jsd = kmer_statistics(kmer_len, data1, data2)
+'''
+
+def kmer_statistics(kmer_length, data1, data2):
+
+    #generate kmer distributions 
+    dist1 = compute_kmer_spectra(data1, kmer_length)
+    dist2 = compute_kmer_spectra(data2, kmer_length)
+
+    #computer KLD
+    kld = np.round(np.sum(scipy.special.kl_div(dist1, dist2)), 6)
+
+    #computer jensen-shannon 
+    jsd = np.round(np.sum(scipy.spatial.distance.jensenshannon(dist1, dist2)), 6)
+
+    return kld, jsd
+
+def compute_kmer_spectra(
+    X,
+    kmer_length=3,
+    dna_dict = {
+        0: "A",
+        1: "C",
+        2: "G",
+        3: "T"
+      }
+    ):
+    # convert one hot to A,C,G,T
+    seq_list = []
+
+    for index in tqdm(range(len(X))): #for loop is what actually converts a list of one-hot encoded sequences into ACGT
+
+        seq = X[index]
+
+        seq_list += ["".join([dna_dict[np.where(i)[0][0]] for i in seq])]
+
+    obj = kmer_featurization(kmer_length)  # initialize a kmer_featurization object
+    kmer_features = obj.obtain_kmer_feature_for_a_list_of_sequences(seq_list, write_number_of_occurrences=True)
+
+    kmer_permutations = ["".join(p) for p in product(["A", "C", "G", "T"], repeat=kmer_length)] #list of all kmer permutations, length specified by repeat=
+
+    kmer_dict = {}
+    for kmer in kmer_permutations:
+        n = obj.kmer_numbering_for_one_kmer(kmer)
+        kmer_dict[n] = kmer
+
+    global_counts = np.sum(np.array(kmer_features), axis=0)
+
+    # what to compute entropy against
+    global_counts_normalized = global_counts / sum(global_counts) # this is the distribution of kmers in the testset
+    # print(global_counts_normalized)
+    return global_counts_normalized
+
+class kmer_featurization:
+
+    def __init__(self, k):
+        """
+        seqs: a list of DNA sequences
+        k: the "k" in k-mer
+        """
+        self.k = k
+        self.letters = ['A', 'C', 'G', 'T']
+        self.multiplyBy = 4 ** np.arange(k-1, -1, -1) # the multiplying number for each digit position in the k-number system
+        self.n = 4**k # number of possible k-mers
+
+    def obtain_kmer_feature_for_a_list_of_sequences(self, seqs, write_number_of_occurrences=False):
+        """
+        Given a list of m DNA sequences, return a 2-d array with shape (m, 4**k) for the 1-hot representation of the kmer features.
+        Args:
+          write_number_of_occurrences:
+            a boolean. If False, then in the 1-hot representation, the percentage of the occurrence of a kmer will be recorded; otherwise the number of occurrences will be recorded. Default False.
+        """
+        kmer_features = [] #a list containing the one-hot representation of kmers for each sequence in the list of sequences given
+        for seq in seqs: #first obtain the one-hot representation of the kmers in a sequence
+            this_kmer_feature = self.obtain_kmer_feature_for_one_sequence(seq.upper(), write_number_of_occurrences=write_number_of_occurrences)
+            kmer_features.append(this_kmer_feature) #append this one-hot list into another list
+
+        kmer_features = np.array(kmer_features)
+
+        return kmer_features
+
+    def obtain_kmer_feature_for_one_sequence(self, seq, write_number_of_occurrences=False): #
+        """
+        Given a DNA sequence, return the 1-hot representation of its kmer feature.
+        Args:
+          seq:
+            a string, a DNA sequence
+          write_number_of_occurrences:
+            a boolean. If False, then in the 1-hot representation, the percentage of the occurrence of a kmer will be recorded; otherwise the number of occurrences will be recorded. Default False.
+        """
+        number_of_kmers = len(seq) - self.k + 1
+
+        kmer_feature = np.zeros(self.n) #array of zeroes the same length of all possible kmers
+
+        for i in range(number_of_kmers): #for each kmer feature, turn the corresponding index in the list of all kmer features to 1
+            this_kmer = seq[i:(i+self.k)]
+            this_numbering = self.kmer_numbering_for_one_kmer(this_kmer)
+            kmer_feature[this_numbering] += 1
+
+        if not write_number_of_occurrences:
+            kmer_feature = kmer_feature / number_of_kmers
+
+        return kmer_feature
+
+    def kmer_numbering_for_one_kmer(self, kmer): #returns the corresponding index of a kmer in the larger list of all possible kmers?
+        """
+        Given a k-mer, return its numbering (the 0-based position in 1-hot representation)
+        """
+        digits = []
+        for letter in kmer:
+            digits.append(self.letters.index(letter))
+
+        digits = np.array(digits)
+
+        numbering = (digits * self.multiplyBy).sum()
+
+        return numbering
